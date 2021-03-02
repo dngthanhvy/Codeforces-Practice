6f8eab077aaccf85644ac1f07ba060e0dfee985e
Thanh-Vy Duong <dng.thanhvy@gmail.com>
2021-03-02 13:25:35 +0100

    added 282A

--- 282A.py
+++ 282A.py
@@ -1,3 +1,10 @@
+# A statement in language Bit++ is a sequence, consisting of exactly one operation and one variable x. The statement
+# is written without spaces, that is, it can only contain characters "+", "-", "X". Executing a statement means
+# applying the operation it contains.
+# ++X or X++ = add 1
+# --X or X-- = sub 1
+# Initial value of X : 0
+
 n = int(input())
 x = 0
 
